6eacd46ba71b84463f960dbe1647b399c0dc3aae
AKASHCS50 <akcount121@gmail.com>
2019-10-06 12:20:03 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx = 1,dirny = 0, color = (255,0)):
+        pass
+    def move(self)
+    
